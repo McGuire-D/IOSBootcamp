5f339361a36672b918db912820fcce192f9032fe
Dustin McGuire <mcguire.dustin@att.net>
2020-10-28 21:40:27 -0500

    Udemy Bootcamp

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Dustin McGuire on 10/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
