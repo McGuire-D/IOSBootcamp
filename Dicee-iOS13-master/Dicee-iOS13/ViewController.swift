dad54acc4b641eafd0b957c6db2bdcd4bee5812d
Mcguire-D <mcguire.dustin@att.net>
2020-11-22 12:49:14 -0600

    Udemy

--- Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
@@ -13,23 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
 // Roll button action 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("leftDiceNumber at beginning = \(leftDiceNumber)")
-        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix") ] [leftDiceNumber]
         
-        diceImageView2.image = [ #imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
-        leftDiceNumber = leftDiceNumber + 1
-        rightDiceNumber = rightDiceNumber - 1
-        print("leftDiceNumber at end = \(leftDiceNumber)")
+        //constant Array 
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix") ]
+
+        //two ways of reaching tandom int's
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
     }
     
 }
