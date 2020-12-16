b9e6e30f4db22fa4e3671628682e7ab0fac94373
Mcguire-D <mcguire.dustin@att.net>
2020-12-15 21:19:08 -0600

    Udemy

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes = ["Soft":5, "Medium":7,"Hard":12]
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle
+        
+        switch hardness{
+        case "Soft":
+            print(eggTimes["Soft"]!)
+        case "Medium":
+            print(eggTimes["Medium"]!)
+        case "Hard":
+            print(eggTimes["Hard"]!)
+        default:
+            print("Whoopsy Daisy something fucked up")
+        }
+//        if hardness == "Soft"{
+//        print(softTime)} else if hardness == "Medium" {
+//        print(mediumTime)} else if hardness == "Hard"{ print(hardTime)}
+    }
 }
+
+
+    
+
