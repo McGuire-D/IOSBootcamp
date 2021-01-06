21f03cc0c0762df5e1ce3bc614052d8c1aa3d073
Mcguire-D <mcguire.dustin@att.net>
2021-01-05 21:17:54 -0600

    Udacity

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -11,17 +11,16 @@ import UIKit
 class ViewController: UIViewController {
     
     let eggTimes = ["Soft":300, "Medium":420,"Hard":720]
-    
-    var secondsRemaining = 60
-    
     var timer = Timer()
-    
+    var totalTime = 0
+    var secondsPassed = 0
+    var secondsRemaining = 0
     @IBOutlet weak var TimeCounter: UILabel!
     @IBOutlet weak var EggLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         EggLabel.text = "How do you like your eggs?"
-        
         timer.invalidate()
         
         let hardness = sender.currentTitle! //Soft, Medium, Hard
@@ -33,14 +32,14 @@ class ViewController: UIViewController {
         }
     
     @objc func updateTimer(){
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds.")
-            TimeCounter.text = String(secondsRemaining)
-            secondsRemaining -= 1
+        if secondsPassed < totalTime {
+            
+            let percentageProgress = secondsPassed / totalTime
+            progressBar.progress = Float(percentageProgress)
+            secondsPassed += 1
         } else {
             timer.invalidate()
             self.EggLabel.text = "Done!"
-            TimeCounter.text = "Times Up!"
         }
     }
 }
