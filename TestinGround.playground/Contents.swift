b1c0c2f04f48f0ce8fe324e79ce1529697b5a561
Mcguire-D <mcguire.dustin@att.net>
2022-01-18 20:21:56 -0600

    Playground

--- TestinGround.playground/Contents.swift
+++ TestinGround.playground/Contents.swift
@@ -1,51 +1,104 @@
 import UIKit
-
-var greeting = "Hello, playground"
-
-
-func runNumbers(){
-    let numbers = [45, 19, 22, 40, 80]
-    
-    let calcNumbers = [numbers[0] * numbers[1], numbers[1] * numbers[2], numbers[2] * numbers[3], numbers[3] * numbers[4], numbers[4] * numbers[0]]
-    
-    print(calcNumbers)
-}
-
-runNumbers()
-
-
-
-
-let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-  
-  //The number of letters in alphabet equals 26
-  
-  let password = alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)]
-  
-  print(password)
-
 /*
-func createPassword(){
-    let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+ var greeting = "Hello, playground"
+ 
+ 
+ func runNumbers(){
+ let numbers = [45, 19, 22, 40, 80]
+ 
+ let calcNumbers = [numbers[0] * numbers[1], numbers[1] * numbers[2], numbers[2] * numbers[3], numbers[3] * numbers[4], numbers[4] * numbers[0]]
+ 
+ print(calcNumbers)
+ }
+ 
+ runNumbers()
+ 
+ 
+ 
+ 
+ let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+ 
+ //The number of letters in alphabet equals 26
+ 
+ let password = alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)] + alphabet[Int.random(in: 0...25)]
+ 
+ print(password)
+ 
+ 
+ func createPassword(){
+ let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+ 
+ var randomPassword = [letters.randomElement() + letters.randomElement() + letters.randomElement() + letters.randomElement() + letters.randomElement() + letters.randomElement()]
+ 
+ print(randomPassword)
+ }
+ 
+ 
+ createPassword()
+ */
+/*
+ let alphabet1 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+ 
+ //The number of letters in alphabet equals 26
+ var strings: String = ""
+ 
+ while strings.count < 6 {
+ strings += alphabet1.randomElement()!
+ }
+ 
+ let password1 = strings
+ 
+ print(password1)
+ */
+
+func goToStore(){
     
-    var randomPassword = [letters.randomElement() + letters.randomElement() + letters.randomElement() + letters.randomElement() + letters.randomElement() + letters.randomElement()]
+    print("LeaveHouse")
     
-    print(randomPassword)
+    for _ in 1 ... 2 {
+        print("moveRight")
     }
-
-
-createPassword()
-*/
-
-let alphabet1 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
-//The number of letters in alphabet equals 26
-var strings: String = ""
-
-while strings.count < 6 {
-    strings += alphabet1.randomElement()!
+    for _ in 1 ... 4 {
+        print("moveUp")
+    }
+    for _ in 1 ... 2 {
+        print("moveRight")
+    }
+    
+    print("buyMilk")
+    
+    for _ in 1 ... 2 {
+        print("moveLeft")
+    }
+    for _ in 1 ... 4 {
+        print("moveDown")
+    }
+    for _ in 1 ... 2 {
+        print("moveLeft")
+    }
+    
+    print("enterHouse")
+    
 }
 
-let password1 = strings
+func greeting(){
+    for _ in 1 ... 4 {
+        print("Hello")
+    }
+}
 
-print(password1)
+goToStore()
+greeting()
+
+
+func greeting1(){
+    let myName = "Angela"
+    print("Hello \(myName)")
+    
+    func greeting2() {
+        print("Hey")
+    }
+    greeting2()
+}
+
+greeting1()
