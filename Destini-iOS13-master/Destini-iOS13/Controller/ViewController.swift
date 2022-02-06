ceaf21a1e4b7ee3c38ae1ef0815ebd31aeea6ba7
Mcguire-D <mcguire.dustin@att.net>
2022-02-06 12:37:04 -0600

    Destini

--- Destini-iOS13-master/Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13-master/Destini-iOS13/Controller/ViewController.swift
@@ -10,26 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let story0 = "You see a fork in the road"
-    let choice1 = "Take a left"
-    let choice2 = "Take a right"
-
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = story0
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
+        updateUI()
         
-
+        
     }
-    @IBAction func choiceMade(_ sender: Any) {
-       
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+                
+                updateUI()
     }
     
-
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
 }
 
